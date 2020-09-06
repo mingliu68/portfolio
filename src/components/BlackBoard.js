6c70752c48ab847b7da6b7dddba268e6cc1567c8
mingliu68 <mingliudev@gmail.com>
2020-09-06 15:04:13 -0400

    add data for positions and projects, modify styles, code refactor

--- src/components/BlackBoard.js
+++ src/components/BlackBoard.js
@@ -1,11 +1,12 @@
 import React, { useState, useCallback } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAngleDoubleDown, faAngleDoubleUp } from '@fortawesome/free-solid-svg-icons'
+import { faAngleDoubleDown, faAngleDoubleUp } from '@fortawesome/free-solid-svg-icons';
+import { project_position, projects } from '../data';
 
 
 const BlackBoard = () => {
     const [status, setStatus] = useState(false);
-    const [mouseOver, setMouseOver] = useState(6);
+    const [mouseOver, setMouseOver] = useState(projects.length);
     const [enlarge, setEnlarge] = useState(0)
 
     const styles = {
@@ -39,7 +40,6 @@ const BlackBoard = () => {
             bottom: -60,
             right: 0,
         },
-
     }
 
     const setBlackBoard = useCallback(() => {
@@ -76,20 +76,21 @@ const BlackBoard = () => {
                     }}
                 />
             </div>
+
             <div className="project-container">
                 <div
                     className="project"
                     style={{
                         borderColor: (enlarge == 1 ? "black" : "#333"),
-                        left: "35%",
-                        top: "3%",
+                        left: project_position[0].left,
+                        top: project_position[0].top,
                         zIndex: (mouseOver == 1 ? 15 : 10),
-                        transform: (enlarge == 1 ? "rotate(0deg) scale(1.25) translate(0, 35px) translateZ(0)" : "rotate(-4deg) scale(1)"),
+                        transform: (enlarge == 1 ? project_position[0].transformPost : project_position[0].transformPre),
                     }}
                     onMouseEnter={() => { setMouseOver(1); setEnlarge(1) }}
                     onMouseLeave={() => setEnlarge(0)}
                 >
-                    <img src="https://www.brooksidemediaco.com/wp-content/uploads/2020/09/Screen-Shot-2020-09-05-at-11.34.50-PM.png" style={{ opacity: (mouseOver == 1 ? 1 : 0.65), }} />
+                    <img src="https://www.brooksidemediaco.com/wp-content/uploads/2020/09/portfolio_home.png" style={{ opacity: (mouseOver == 1 ? 1 : 0.65), }} />
                     <div style={{ paddingTop: 15, color: (mouseOver == 1 ? "#333" : "#ccc") }}>Personal Portfolio - React App</div>
                 </div>
 
@@ -97,10 +98,10 @@ const BlackBoard = () => {
                     className="project"
                     style={{
                         borderColor: (enlarge == 2 ? "black" : "#333"),
-                        left: "5%",
-                        top: "8%",
+                        left: project_position[1].left,
+                        top: project_position[1].top,
                         zIndex: (mouseOver == 2 ? 15 : 10),
-                        transform: (enlarge == 2 ? "rotate(0deg) scale(1.25) translate(25px, 10px) translateZ(0)" : "rotate(10deg) scale(1)"),
+                        transform: (enlarge == 2 ? project_position[1].transformPost : project_position[1].transformPre),
                     }}
                     onMouseEnter={() => { setMouseOver(2); setEnlarge(2) }}
                     onMouseLeave={() => setEnlarge(0)}
@@ -113,10 +114,10 @@ const BlackBoard = () => {
                     className="project"
                     style={{
                         borderColor: (enlarge == 3 ? "black" : "#333"),
-                        left: "4%",
-                        bottom: "8%",
+                        left: project_position[2].left,
+                        bottom: project_position[2].bottom,
                         zIndex: (mouseOver == 3 ? 15 : 10),
-                        transform: (enlarge == 3 ? "rotate(0deg) scale(1.25) translate(25px, -10px) translateZ(0)" : "rotate(-7deg) scale(1)"),
+                        transform: (enlarge == 3 ? project_position[2].transformPost : project_position[2].transformPre),
                     }}
                     onMouseEnter={() => { setMouseOver(3); setEnlarge(3) }}
                     onMouseLeave={() => setEnlarge(0)}
@@ -129,10 +130,10 @@ const BlackBoard = () => {
                     className="project"
                     style={{
                         borderColor: (enlarge == 4 ? "black" : "#333"),
-                        right: "7%",
-                        top: "15%",
+                        right: project_position[3].right,
+                        top: project_position[3].top,
                         zIndex: (mouseOver == 4 ? 15 : 10),
-                        transform: (enlarge == 4 ? "rotate(0deg) scale(1.25) translateZ(0)" : "rotate(-5deg) scale(1)")
+                        transform: (enlarge == 4 ? project_position[3].transformPost : project_position[3].transformPre)
                     }}
                     onMouseEnter={() => { setMouseOver(4); setEnlarge(4) }}
                     onMouseLeave={() => setEnlarge(0)}
@@ -146,14 +147,13 @@ const BlackBoard = () => {
                     className="project"
                     style={{
                         borderColor: (enlarge == 5 ? "black" : "#333"),
-                        right: "5%",
-                        bottom: "8%",
+                        right: project_position[4].right,
+                        bottom: project_position[4].bottom,
                         zIndex: (mouseOver == 5 ? 15 : 10),
-                        transform: (enlarge == 5 ? "rotate(0deg) scale(1.25) translate(-25px, -10px) translateZ(0)" : "rotate(5deg) scale(1)"),
+                        transform: (enlarge == 5 ? project_position[4].transformPost : project_position[4].transformPre),
                     }}
                     onMouseEnter={() => { setMouseOver(5); setEnlarge(5) }}
                     onMouseLeave={() => setEnlarge(0)}
-
                 >
                     <img src="https://www.brooksidemediaco.com/wp-content/uploads/2020/09/skinDecoded_2.png" style={{ opacity: (mouseOver == 5 ? 1 : 0.65) }} />
                     <div style={{ paddingTop: 15, color: (mouseOver == 5 ? "#333" : "#ccc") }}>eCommerce Site - PrestaShop</div>
@@ -163,10 +163,10 @@ const BlackBoard = () => {
                     className="project"
                     style={{
                         borderColor: (enlarge == 6 ? "black" : "#333"),
-                        left: "25%",
-                        bottom: "5%",
+                        left: project_position[5].left,
+                        bottom: project_position[5].bottom,
                         zIndex: (mouseOver == 6 ? 15 : 10),
-                        transform: (enlarge == 6 ? "rotate(0deg) scale(1.25) translate(0, -25px) translateZ(0)" : "rotate(3deg) scale(1)"),
+                        transform: (enlarge == 6 ? project_position[5].transformPost : project_position[5].transformPre),
                     }}
                     onMouseEnter={() => { setMouseOver(6); setEnlarge(6) }}
                     onMouseLeave={() => setEnlarge(0)}
--- src/data.js
+++ src/data.js
@@ -13,15 +13,94 @@ export const skillset = [
     }
 ]
 
+export const project_position = [
+    {
+        top: "3%",
+        bottom: null,
+        left: "35%",
+        right: null,
+        transformPost: "rotate(0deg) scale(1.25) translate(0, 35px) translateZ(0)", //transformPost => transforming
+        transformPre: "rotate(-4deg) scale(1)",  // transformPre => return to original
+    },
+    {
+        top: "8%",
+        bottom: null,
+        left: "5%",
+        right: null,
+        transformPost: "rotate(0deg) scale(1.25) translate(25px, 10px) translateZ(0)", //transformPost => transforming
+        transformPre: "rotate(10deg) scale(1)",  // transformPre => return to original
+    },
+    {
+        top: null,
+        bottom: "8%",
+        left: "4%",
+        right: null,
+        transformPost: "rotate(0deg) scale(1.25) translate(25px, -10px) translateZ(0)", //transformPost => transforming
+        transformPre: "rotate(-7deg) scale(1)",  // transformPre => return to original
+    },
+    {
+        top: "15%",
+        bottom: null,
+        left: null,
+        right: "7%",
+        transformPost: "rotate(0deg) scale(1.25) translateZ(0)", //transformPost => transforming
+        transformPre: "rotate(-5deg) scale(1)",  // transformPre => return to original
+    },
+    {
+        top: null,
+        bottom: "8%",
+        left: null,
+        right: "5%",
+        transformPost: "rotate(0deg) scale(1.25) translate(-25px, -10px) translateZ(0)", //transformPost => transforming
+        transformPre: "rotate(5deg) scale(1)",  // transformPre => return to original
+    },
+    {
+        top: null,
+        bottom: "5%",
+        left: "25%",
+        right: null,
+        transformPost: "rotate(0deg) scale(1.25) translate(0, -25px) translateZ(0)", //transformPost => transforming
+        transformPre: "rotate(3deg) scale(1)",  // transformPre => return to original
+    },
+
+]
+
 export const projects = [
     {
+        name: "Personal Portfolio",
+        display: "Personal Portfolio - React App",
+        tech: ["React", "JavaScript", "HTML5", "CSS3",],
+        img: "https://www.brooksidemediaco.com/wp-content/uploads/2020/09/portfolio_home.png"
+    },
+    {
         name: "Sarama Animal Rescue",
+        display: "Sarama Rescue - WordPress Site",
         tech: ["PHP", "JavaScript", "HTML5", "CSS3", "WordPress", "WooCommerce", "JQuery"],
         img: "http://www.brooksidemediaco.com/wp-content/uploads/2018/05/sarama_home.png",
     },
     {
+        name: "PawsnFind",
+        display: "PawsnFind - React App",
+        tech: [],
+        img: "https://www.brooksidemediaco.com/wp-content/uploads/2020/09/Screen-Shot-2020-09-05-at-4.22.10-PM.png"
+    },
+    {
         name: "Brookside Media & Co.",
+        display: "Brookside Media - WordPress Site",
         tech: ["PHP", "JavaScript", "HTML5", "CSS3", "WordPress", "WooCommerce", "JQuery"],
         img: "https://www.brooksidemediaco.com/wp-content/uploads/2020/09/brooksidemedia_home.png",
     },
+    {
+        name: "SkinDecoded",
+        display: "eCommerce Site - PrestaShop",
+        tech: ["PHP", "JavaScript", "HTML5", "CSS3", "PrestaShop", "JQuery"],
+        img: "https://www.brooksidemediaco.com/wp-content/uploads/2020/09/skinDecoded_2.png",
+    },
+    {
+        name: "Peach&Lemon",
+        display: "eCommerce Site - PrestaShop",
+        tech: ["PHP", "JavaScript", "HTML5", "CSS3", "PrestaShop", "JQuery"],
+        img: "https://www.brooksidemediaco.com/wp-content/uploads/2020/09/peachLemonHome.png",
+    },
+
 ]
