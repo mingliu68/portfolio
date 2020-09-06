c31569a889ef641a45d1fb7180ee0be5e99ee802
mingliu68 <mingliudev@gmail.com>
2020-09-06 15:31:45 -0400

    code refactor

--- src/components/BlackBoard.js
+++ src/components/BlackBoard.js
@@ -2,6 +2,7 @@ import React, { useState, useCallback } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleDoubleDown, faAngleDoubleUp } from '@fortawesome/free-solid-svg-icons';
 import { project_position, projects } from '../data';
+import ProjectBox from './ProjectBox';
 
 
 const BlackBoard = () => {
@@ -78,104 +79,21 @@ const BlackBoard = () => {
             </div>
 
             <div className="project-container">
-                <div
-                    className="project"
-                    style={{
-                        borderColor: (enlarge == 1 ? "black" : "#333"),
-                        left: project_position[0].left,
-                        top: project_position[0].top,
-                        zIndex: (mouseOver == 1 ? 15 : 10),
-                        transform: (enlarge == 1 ? project_position[0].transformPost : project_position[0].transformPre),
-                    }}
-                    onMouseEnter={() => { setMouseOver(1); setEnlarge(1) }}
-                    onMouseLeave={() => setEnlarge(0)}
-                >
-                    <img src="https://www.brooksidemediaco.com/wp-content/uploads/2020/09/portfolio_home.png" style={{ opacity: (mouseOver == 1 ? 1 : 0.65), }} />
-                    <div style={{ paddingTop: 15, color: (mouseOver == 1 ? "#333" : "#ccc") }}>Personal Portfolio - React App</div>
-                </div>
-
-                <div
-                    className="project"
-                    style={{
-                        borderColor: (enlarge == 2 ? "black" : "#333"),
-                        left: project_position[1].left,
-                        top: project_position[1].top,
-                        zIndex: (mouseOver == 2 ? 15 : 10),
-                        transform: (enlarge == 2 ? project_position[1].transformPost : project_position[1].transformPre),
-                    }}
-                    onMouseEnter={() => { setMouseOver(2); setEnlarge(2) }}
-                    onMouseLeave={() => setEnlarge(0)}
-
-                >
-                    <img src="http://www.brooksidemediaco.com/wp-content/uploads/2018/05/sarama_home.png" style={{ opacity: (mouseOver == 2 ? 1 : 0.65), }} />
-                    <div style={{ paddingTop: 15, color: (mouseOver == 2 ? "#333" : "#ccc") }}>Sarama Rescue - WordPress Site</div>
-                </div>
-                <div
-                    className="project"
-                    style={{
-                        borderColor: (enlarge == 3 ? "black" : "#333"),
-                        left: project_position[2].left,
-                        bottom: project_position[2].bottom,
-                        zIndex: (mouseOver == 3 ? 15 : 10),
-                        transform: (enlarge == 3 ? project_position[2].transformPost : project_position[2].transformPre),
-                    }}
-                    onMouseEnter={() => { setMouseOver(3); setEnlarge(3) }}
-                    onMouseLeave={() => setEnlarge(0)}
-                >
-                    <img src="https://www.brooksidemediaco.com/wp-content/uploads/2020/09/Screen-Shot-2020-09-05-at-4.22.10-PM.png" alt="pawsnfind" style={{ opacity: (mouseOver == 3 ? 1 : 0.65) }} />
-                    <div style={{ paddingTop: 15, color: (mouseOver == 3 ? "#333" : "#ccc") }}>PawsnFind - React App</div>
-
-                </div>
-                <div
-                    className="project"
-                    style={{
-                        borderColor: (enlarge == 4 ? "black" : "#333"),
-                        right: project_position[3].right,
-                        top: project_position[3].top,
-                        zIndex: (mouseOver == 4 ? 15 : 10),
-                        transform: (enlarge == 4 ? project_position[3].transformPost : project_position[3].transformPre)
-                    }}
-                    onMouseEnter={() => { setMouseOver(4); setEnlarge(4) }}
-                    onMouseLeave={() => setEnlarge(0)}
-                >
-
-                    <img src="https://www.brooksidemediaco.com/wp-content/uploads/2020/09/brooksidemedia_home.png" style={{ opacity: (mouseOver == 4 ? 1 : 0.65) }} />
-                    <div style={{ paddingTop: 15, color: (mouseOver == 4 ? "#333" : "#ccc") }}>Brookside Media - WordPress Site</div>
-
-                </div>
-                <div
-                    className="project"
-                    style={{
-                        borderColor: (enlarge == 5 ? "black" : "#333"),
-                        right: project_position[4].right,
-                        bottom: project_position[4].bottom,
-                        zIndex: (mouseOver == 5 ? 15 : 10),
-                        transform: (enlarge == 5 ? project_position[4].transformPost : project_position[4].transformPre),
-                    }}
-                    onMouseEnter={() => { setMouseOver(5); setEnlarge(5) }}
-                    onMouseLeave={() => setEnlarge(0)}
-                >
-                    <img src="https://www.brooksidemediaco.com/wp-content/uploads/2020/09/skinDecoded_2.png" style={{ opacity: (mouseOver == 5 ? 1 : 0.65) }} />
-                    <div style={{ paddingTop: 15, color: (mouseOver == 5 ? "#333" : "#ccc") }}>eCommerce Site - PrestaShop</div>
-
-                </div>
-                <div
-                    className="project"
-                    style={{
-                        borderColor: (enlarge == 6 ? "black" : "#333"),
-                        left: project_position[5].left,
-                        bottom: project_position[5].bottom,
-                        zIndex: (mouseOver == 6 ? 15 : 10),
-                        transform: (enlarge == 6 ? project_position[5].transformPost : project_position[5].transformPre),
-                    }}
-                    onMouseEnter={() => { setMouseOver(6); setEnlarge(6) }}
-                    onMouseLeave={() => setEnlarge(0)}
-                >
-                    <img src="https://www.brooksidemediaco.com/wp-content/uploads/2020/09/peachLemonHome.png" style={{ opacity: (mouseOver == 6 ? 1 : 0.65) }} />
-                    <div style={{ paddingTop: 15, color: (mouseOver == 6 ? "#333" : "#ccc") }}>eCommerce Site - PrestaShop</div>
-
-                </div>
-
+                {
+                    projects.map((project, index) => {
+                        return (
+                            <ProjectBox
+                                mouseOver={mouseOver}
+                                setMouseOver={setMouseOver}
+                                enlarge={enlarge}
+                                setEnlarge={setEnlarge}
+                                project_position={project_position[index]}
+                                project={project}
+                                index={index}
+                            />
+                        )
+                    })
+                }
             </div>
             <h1 style={styles.title}>
                 Ming Liu
---
+++ src/components/ProjectBox.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const ProjectBox = (props) => {
+    const { project_position, project, mouseOver, setMouseOver, enlarge, setEnlarge, index } = { ...props }
+    return (
+        <div
+            className="project"
+            style={{
+                borderColor: (enlarge == index + 1 ? "black" : "#333"),
+                right: project_position.right,
+                left: project_position.left,
+                top: project_position.top,
+                bottom: project_position.bottom,
+                zIndex: (mouseOver == index + 1 ? 15 : 10),
+                transform: (enlarge == index + 1 ? project_position.transformPost : project_position.transformPre),
+            }}
+            onMouseEnter={() => { setMouseOver(index + 1); setEnlarge(index + 1) }}
+            onMouseLeave={() => setEnlarge(0)}
+        >
+            <img src={project.img} style={{ opacity: (mouseOver == index + 1 ? 1 : 0.65) }} />
+            <div style={{ paddingTop: 15, color: (mouseOver == index + 1 ? "#333" : "#ccc") }}>{project.display}</div>
+        </div>
+    )
+}
+
+export default ProjectBox;
