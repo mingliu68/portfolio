98dd898964f8374bb255bd5afe93d1544174ef65
mingliu68 <mingliudev@gmail.com>
2020-09-09 16:51:23 -0400

    set up dials for initial time and rotations

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -2,11 +2,20 @@ import React, { useState, useEffect, useCallback } from "react";
 import Dial from './Dial';
 
 const Clock = () => {
+    const today = new Date();
+    const hour = today.getHours() % 12
+    const min = today.getMinutes()
+    const sec = today.getSeconds()
 
     return (
         <div style={{ position: "absolute", width: 200, height: 200, right: 50, top: 150, backgroundColor: "white" }}>
             <div style={{ position: "relative", width: "100%", height: "100%" }} >
-                <Dial />
+                <div>{hour % 12} {min} {sec}</div>
+
+
+                <Dial hour={hour} min={min} sec={sec} type="second" />
+                <Dial hour={hour} min={min} sec={sec} type="minute" />
+                <Dial hour={hour} min={min} sec={sec} type="hour" />
             </div>
         </div>
     )
--- src/components/Dial.js
+++ src/components/Dial.js
@@ -1,36 +1,62 @@
 import React, { useState, useEffect } from "react";
 
-const Dial = () => {
-    let temp = -90
-    const [secDial, setSecDial] = useState(temp)
+const Dial = (props) => {
+    const { hour, min, type, sec } = { ...props }
+    console.log(hour, min, sec, type)
 
-
-    const secDialCounter = () => {
-        function sec() {
-            temp = temp + 6 === 270 ? -90 : temp + 6
-            setSecDial(temp)
-            console.log(temp)
+    let setup = {};
+    (() => {
+        if (type === "second") {
+            setup = {
+                temp: -90 + (6 * sec),
+                intervalTime: 1000,
+                intervalRotate: 6
+            }
+        } else if (type === "minute") {
+            setup = {
+                temp: -90 + (6 * min),
+                intervalTime: 60000,
+                intervalRotate: 6
+            }
+        } else if (type === "hour") {
+            setup = {
+                temp: -90 + (30 * hour),
+                intervalTime: 600000,
+                intervalRotate: 5
+            }
         }
-        setInterval(sec, 1000)
+    })()
+
+    const [dial, setDial] = useState(setup.temp)
+
+    const startDial = () => {
+
+        function dialRotation() {
+            setup.temp = setup.temp + setup.intervalRotate === 270 ? -90 : setup.temp + setup.intervalRotate;
+            setDial(setup.temp)
+            console.log(type, setup.temp)
+        }
+        setInterval(dialRotation, setup.intervalTime)
     }
 
     useEffect(() => {
-        secDialCounter()
+        startDial()
+        console.log(type, setup.temp)
     }, [])
 
     return (
         <div
             style={{
-                width: 80,
-                borderWidth: 2,
-                borderColor: "black",
+                width: (type === "hour" ? 60 : 80),
+                borderWidth: (type === "second" ? 2 : type === "minute" ? 3 : 4),
+                borderColor: (type === "hour" ? "red" : "black"),
                 borderStyle: "solid",
                 position: "absolute",
                 left: 100,
                 top: 100,
                 backgroundColor: "black",
                 transformOrigin: "left",
-                transform: `rotate(${secDial}deg)`
+                transform: `rotate(${dial}deg)`
             }}
         />
     )
